9939b49110a6ef2407077d5ee77b61875d16ddfc
bondarbv <92814470+bondarbv@users.noreply.github.com>
2022-07-12 21:49:59 +0300

    Created descriptionButton func

--- NatifeTestTask/ViewControllers/MainViewController.swift
+++ NatifeTestTask/ViewControllers/MainViewController.swift
@@ -7,10 +7,50 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+final class MainViewController: UIViewController {
     
-    var mainTableView: UITableView!
+    //MARK: - Data for test
+    private let natifes: [NatifeInfoModel] = [
+        NatifeInfoModel(
+            title: "Some title some title some title",
+            description: "Some text some text some text some text some text some text some text some text some text some text some text some text some text Some text some text some text some text some text some text some text some text some text some text some text some text some text",
+            likesTitle: "1957",
+            dateTitle: "21 day ago"),
+        
+        NatifeInfoModel(
+            title: "Some title some title some title",
+            description: "Some text some text some text some text some text some text some text some text some text some text some text some text some text Some text some text some text some text some text some text some text some text some text some text some text some text some text ",
+            likesTitle: "1957",
+            dateTitle: "21 day ago"),
+        
+        NatifeInfoModel(
+            title: "Some title some title some title",
+            description: "Some text some text some text some text Some text some text some text some text Some text",
+            likesTitle: "1957",
+            dateTitle: "21 day ago"),
+        
+        NatifeInfoModel(
+            title: "Some title some title some title",
+            description: "Some text some text some text some text some text some text some text some text some text some text some text some text some text Some text some text some text some text some text some text some text some text some text some text some text some text some text ",
+            likesTitle: "1957",
+            dateTitle: "21 day ago"),
+        
+        NatifeInfoModel(
+            title: "Some title some title some title",
+            description: "Some text some text some text some text",
+            likesTitle: "1957",
+            dateTitle: "21 day ago"),
+        
+        NatifeInfoModel(
+            title: "Some title some title some title",
+            description: "Some text some text some text some text some text some text some text some text some text some text some text some text some text Some text some text some text some text some text some text some text some text some text some text some text some text some text ",
+            likesTitle: "1957",
+            dateTitle: "21 day ago")
+    ]
     
+    private var mainTableView: UITableView!
+    
+    //MARK: - ViewLifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -23,18 +63,20 @@ class MainViewController: UIViewController {
     }
     
     //MARK: - NavigationBar configuration
-    func configureNavigationBar() {
+    private func configureNavigationBar() {
         navigationItem.title = "Natife"
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "filterbutton"),
                                                             style: .done,
                                                             target: self,
                                                             action: #selector(filterTableView))
         navigationItem.rightBarButtonItem?.tintColor = #colorLiteral(red: 0.2274509804, green: 0.3254901961, blue: 0.3647058824, alpha: 1)
-        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.2274509804, green: 0.3254901961, blue: 0.3647058824, alpha: 1)]
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.font: UIFont(name: "SFProDisplay-Bold", size: 20) ?? "",
+            NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.2274509804, green: 0.3254901961, blue: 0.3647058824, alpha: 1)]
     }
     
     //MARK: - Creating a TableView
-    func creatingTableView() {
+    private func creatingTableView() {
         mainTableView = UITableView(frame: view.bounds, style: .plain)
         mainTableView.separatorStyle = .none
         mainTableView.delegate = self
@@ -48,19 +90,34 @@ class MainViewController: UIViewController {
 //MARK: - TableView Delegate & DataSource
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        5
+        natifes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.id)
                 as? CustomTableViewCell else { return UITableViewCell() }
-        cell.titleLabel.text = "Some title some title some title"
-        cell.descriptionLabel.text = "Some text some text some text some text some text some text some text some text some text some text some text some text some text Some text some text some text some text some text some text some text some text some text some text some text some text some text "
-        cell.likesLabel.text = "❤️1957"
-        cell.dateLabel.text = "21 day ago"
+        cell.setupCell(natifeInfoModel: natifes[indexPath.row])
+
+        if cell.descriptionLabel.numberOfLines == 1 {
+            cell.descriptionButton.removeFromSuperview()
+            cell.contentView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        } else if cell.descriptionLabel.numberOfLines == 2 && cell.descriptionLabel.text?.count ?? 0 >= 100 {
+            cell.descriptionButton.removeFromSuperview()
+            cell.contentView.heightAnchor.constraint(equalToConstant: 125).isActive = true
+        }
+        
+        cell.handleState = {
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
         return cell
     }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
-    }
+}
+
+//MARK: - Model for test
+public struct NatifeInfoModel {
+    let title: String
+    let description: String
+    let likesTitle: String
+    let dateTitle: String
 }
--- NatifeTestTask/Views/Cells/CustomTableViewCell.swift
+++ NatifeTestTask/Views/Cells/CustomTableViewCell.swift
@@ -7,51 +7,96 @@
 
 import UIKit
 
-class CustomTableViewCell: UITableViewCell {
+final class CustomTableViewCell: UITableViewCell {
     
+    //MARK: - Cell ID
     static let id = "CustomCell"
     
-    let titleLabel: UILabel = {
+    public var handleState: (() -> Void) = { }
+    
+    private enum DescriptionState {
+        case collapse
+        case expand
+        
+        mutating func toggle() {
+            switch self {
+            case .collapse: self = .expand
+            case .expand: self = .collapse
+            }
+        }
+    }
+    
+    private var descriptionState = DescriptionState.collapse
+    
+    private let titleLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.font = UIFont(name: "SFProDisplay-Bold", size: 25)
         label.textColor = #colorLiteral(red: 0.2775951028, green: 0.3229554296, blue: 0.369166106, alpha: 1)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    let descriptionLabel: UILabel = {
-       let label = UILabel()
-        label.numberOfLines = 2
-        label.textColor = #colorLiteral(red: 0.5298442841, green: 0.5901135802, blue: 0.6659915447, alpha: 1)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    let likesLabel: UILabel = {
+    public let descriptionLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 14)
+        label.numberOfLines = 2
+        label.font = UIFont(name: "SFProDisplay-Regular", size: 18)
         label.textColor = #colorLiteral(red: 0.5298442841, green: 0.5901135802, blue: 0.6659915447, alpha: 1)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    let dateLabel: UILabel = {
-       let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 14)
+    private let likesLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "SFProDisplay-Regular", size: 14)
         label.textColor = #colorLiteral(red: 0.5298442841, green: 0.5901135802, blue: 0.6659915447, alpha: 1)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
-    let descriptionButton: UIButton = {
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "SFProDisplay-Regular", size: 14)
+        label.textColor = #colorLiteral(red: 0.5298442841, green: 0.5901135802, blue: 0.6659915447, alpha: 1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    public let descriptionButton: UIButton = {
         let button = UIButton()
         button.setTitle("Expand", for: .normal)
+        button.titleLabel?.font = UIFont(name: "SFProDisplay-Regular", size: 18)
         button.backgroundColor = #colorLiteral(red: 0.2775951028, green: 0.3229554296, blue: 0.369166106, alpha: 1)
         button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(updateDescriptionLabel), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
+    @objc private func updateDescriptionLabel() {
+        switch descriptionState {
+        case .expand:
+            descriptionButton.setTitle("Expand", for: .normal)
+            heightAnchor.constraint(equalToConstant: 200).isActive = true
+            descriptionLabel.numberOfLines = 2
+            handleState()
+        case .collapse:
+            descriptionButton.setTitle("Collapse", for: .normal)
+            heightAnchor.constraint(equalToConstant: 300).isActive = true
+            descriptionLabel.numberOfLines = 0
+            handleState()
+        }
+        descriptionState.toggle()
+    }
+    
+    //MARK: - Setup Cell
+    func setupCell(natifeInfoModel: NatifeInfoModel) {
+        titleLabel.text = natifeInfoModel.title
+        descriptionLabel.text = natifeInfoModel.description
+        likesLabel.text = "❤️ \(natifeInfoModel.likesTitle)"
+        dateLabel.text = natifeInfoModel.dateTitle
+    }
+    
+    //MARK: - Cell Init
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         layout()
@@ -61,7 +106,8 @@ class CustomTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func layout() {
+    //MARK: - Layout
+    private func layout() {
         contentView.addSubview(titleLabel)
         contentView.addSubview(descriptionLabel)
         contentView.addSubview(likesLabel)
@@ -72,6 +118,7 @@ class CustomTableViewCell: UITableViewCell {
             titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
             titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
             titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -10),
+            titleLabel.heightAnchor.constraint(equalToConstant: 25),
             
             descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
             descriptionLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
@@ -86,8 +133,8 @@ class CustomTableViewCell: UITableViewCell {
             descriptionButton.topAnchor.constraint(equalTo: likesLabel.bottomAnchor, constant: 20),
             descriptionButton.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
             descriptionButton.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
-            descriptionButton.heightAnchor.constraint(equalToConstant: 50)
+            descriptionButton.heightAnchor.constraint(equalToConstant: 50),
+            descriptionButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
-        
     }
 }
